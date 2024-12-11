e26f10148100357920a7ca7d816601039f56d8f1
Anastasiia Paliha <nastya.paliha@gmail.com>
2024-12-11 22:03:54 +0200

    make build

---
+++ index.caa9f9ed.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),o=e.offsetWidth,c=e.offsetHeight,f=t.offsetWidth,s=t.offsetHeight;t.style.cssText="\ntop: ".concat((o-f)/2,"px;\nleft: ").concat((c-s)/2,"px;\n");
+//# sourceMappingURL=index.caa9f9ed.js.map
